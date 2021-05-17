39d943f2139f9ab190585ad05a19ec4c5180ef85
AurelienNioche <nioche.aurelien@gmail.com>
2021-05-17 20:07:36 +0300

    update

--- publish_changes.zsh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh 
-echo "converting notebook into html..."
-jupyter-nbconvert --to html lecture.ipynb --output docs/lecture.html --template toc2
-echo "copying images..."
-cp -r img docs
-echo "done!"
